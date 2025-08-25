71ade52eb19714dc1f4588cefde449bfbd071e03
irond85 <ofocacion.34@gmail.com>
2025-08-25 16:48:13 -0500

    change swagger management

--- infrastructure/entry-points/reactive-web/src/main/java/co/irond/crediya/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/irond/crediya/api/Handler.java
@@ -1,11 +1,16 @@
 package co.irond.crediya.api;
 
-import co.irond.crediya.api.dto.ApiResponse;
+import co.irond.crediya.api.dto.ApiResponseDto;
 import co.irond.crediya.api.dto.UserRegistrationDto;
 import co.irond.crediya.api.utils.UserMapper;
 import co.irond.crediya.api.utils.ValidationService;
 import co.irond.crediya.model.user.User;
 import co.irond.crediya.r2dbc.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -23,20 +28,55 @@ public class Handler {
     private final ValidationService validationService;
     private final UserMapper userMapper;
 
+    @Operation(
+            operationId = "getAllUsers",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "get all users successfully.",
+                            content = @Content(
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    )
+            }
+    )
     public Mono<ServerResponse> listenGETUseCase(ServerRequest request) {
         return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(userService.getAllUsers(), User.class);
     }
 
+    @Operation(
+            operationId = "createUser",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "successful operation",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Fields empty or null",
+                            content = @Content(
+                                    schema = @Schema(implementation = ApiResponseDto.class)
+                            )
+                    ),
+            },
+            requestBody = @RequestBody(
+                    content = @Content(
+                            schema = @Schema(implementation = UserRegistrationDto.class)
+                    )
+            )
+    )
     public Mono<ServerResponse> listenSaveUser(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(UserRegistrationDto.class)
                 .flatMap(validationService::validateObject)
                 .map(userMapper::toUser)
                 .flatMap(userService::saveUser)
                 .flatMap(savedUser -> {
-                    ApiResponse<Object> response = ApiResponse.builder()
+                    ApiResponseDto<Object> response = ApiResponseDto.builder()
                             .status(201)
                             .message("User created successfull")
-                            .path(serverRequest.path())
                             .data(savedUser).build();
                     return ServerResponse.status(201).contentType(MediaType.APPLICATION_JSON).bodyValue(response);
                 });
--- infrastructure/entry-points/reactive-web/src/main/java/co/irond/crediya/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/irond/crediya/api/RouterRest.java
@@ -1,17 +1,9 @@
 package co.irond.crediya.api;
 
-import co.irond.crediya.api.dto.UserRegistrationDto;
-import co.irond.crediya.model.user.User;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -24,56 +16,8 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterRest {
     @Bean
     @RouterOperations({
-            @RouterOperation(
-                    path = "/api/v1/usuarios",
-                    produces = {MediaType.APPLICATION_JSON_VALUE},
-                    method = RequestMethod.GET,
-                    beanClass = Handler.class,
-                    beanMethod = "listenGETUseCase",
-                    operation = @Operation(
-                            operationId = "getAllUsers",
-                            responses = {
-                                    @ApiResponse(
-                                            responseCode = "200",
-                                            description = "get all users successfully.",
-                                            content = @Content(
-                                                    schema = @Schema(implementation = User.class)
-                                            )
-                                    )
-                            }
-                    )
-            ),
-            @RouterOperation(
-                    path = "/api/v1/usuarios",
-                    produces = {MediaType.APPLICATION_JSON_VALUE},
-                    method = RequestMethod.POST,
-                    beanClass = Handler.class,
-                    beanMethod = "listenSaveUser",
-                    operation = @Operation(
-                            operationId = "createUser",
-                            responses = {
-                                    @ApiResponse(
-                                            responseCode = "201",
-                                            description = "successful operation",
-                                            content = @Content(
-                                                    schema = @Schema(implementation = co.irond.crediya.api.dto.ApiResponse.class)
-                                            )
-                                    ),
-                                    @ApiResponse(
-                                            responseCode = "400",
-                                            description = "Fields empty or null",
-                                            content = @Content(
-                                                    schema = @Schema(implementation = co.irond.crediya.api.dto.ApiResponse.class)
-                                            )
-                                    ),
-                            },
-                            requestBody = @RequestBody(
-                                    content = @Content(
-                                            schema = @Schema(implementation = UserRegistrationDto.class)
-                                    )
-                            )
-                    )
-            )
+            @RouterOperation(path = "/api/v1/usuarios", method = RequestMethod.GET, beanClass = Handler.class, beanMethod = "listenGETUseCase"),
+            @RouterOperation(path = "/api/v1/usuarios", method = RequestMethod.POST, beanClass = Handler.class, beanMethod = "listenSaveUser")
     })
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
         return route(GET("/api/v1/usuarios"), handler::listenGETUseCase)
