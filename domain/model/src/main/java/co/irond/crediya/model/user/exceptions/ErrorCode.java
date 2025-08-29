ca81161a2423fa3d49b736784c213823b9ff6827
irond85 <ofocacion.34@gmail.com>
2025-08-28 23:22:55 -0500

    update business code for enum

--- domain/model/src/main/java/co/irond/crediya/model/user/exceptions/ErrorCode.java
+++ domain/model/src/main/java/co/irond/crediya/model/user/exceptions/ErrorCode.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    EMAIL_ALREADY_EXISTS("B_001", "Already exists a user with this email.", 409),
-    INVALID_BASE_SALARY("B_002", "The base salary is not within range.", 422),
-    USER_NOT_FOUND("B_003", "The user with dni doesn't exists.", 404),
-    DNI_ALREADY_EXISTS("B_004", "Already exists a user with this dni.", 409);
+    EMAIL_ALREADY_EXISTS("BA_AE_001", "Already exists a user with this email.", 409),
+    INVALID_BASE_SALARY("BA_NR_001", "The base salary is not within range.", 422),
+    USER_NOT_FOUND("BA_NF_001", "The user with dni doesn't exists.", 404),
+    DNI_ALREADY_EXISTS("BA_AE_002", "Already exists a user with this dni.", 409);
 
     private final String internCode;
     private final String message;
